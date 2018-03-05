abd98fa00f29a7ff0aaa0959c87766c6b56b2f17
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-03-04 23:46:39 -0300

    Finalizando conecção no pg

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const app = express();
 
 var con = new pg.Client(config.database);
 
-con.connect();
-con.query("CREATE TABLE TEST (id serial primary key, name varchar(255));");
-con.query("INSERT INTO TEST VALUES ('Nilton');")
-con.query("SELECT * FROM TEST;")
-
 // Start the server
 const server = app.listen(config.port);
 console.log('Your server is running on port ' + config.port + '.');
@@ -21,3 +16,10 @@ console.log('Your server is running on port ' + config.port + '.');
 app.get('/', function(req, res) {
     res.send('hello world');
 });
+
+con.connect();
+con.query("CREATE TABLE TEST (id serial primary key, name varchar(255));");
+con.query("INSERT INTO TEST VALUES ('Nilton');");
+con.query("SELECT * FROM TEST;");
+
+con.end();
