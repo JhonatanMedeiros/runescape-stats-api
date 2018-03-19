1609db0c63c0149d5af20bdefa31bb2b539d5850
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-03-18 22:36:48 -0300

    Corrigido bug de crash

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
+app.use(function (req, res, next) {
+
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
 
 app.use('/player', PlayerRouter);
--- routes/player.route.js
+++ routes/player.route.js
@@ -5,34 +5,27 @@ const updater = require('../updater');
 
 router.get('/search/:username', (req, res) => {
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
   runeApi.osrs.hiscores.player(req.params.username)
     .then((data) => {
 
       var skills = data.skills;
       res.status(200).send(skills);
 
-
       //TODO Método abaixo é relacioado a consulta ao banco de dados
       // updater.logInfo(data, req.params.username)
-      //     .then(data => {
-      //         console.log(data);
-      //         res.send(data.skills);
-      //     })
-      //     .catch(error => {
-      //         res.status(500);
-      //         res.send({'SQL Error:': error});
-      //     });
+      //   .then(data => {
+      //     console.log(data);
+      //     res.send(data.skills);
+      //   })
+      //   .catch(error => {
+      //     res.status(500);
+      //     res.send({'SQL Error:': error});
+      //   });
 
     })
     .catch((error) => {
       res.status(404).send(error);
     });
-
-
 });
 
 module.exports = router;
